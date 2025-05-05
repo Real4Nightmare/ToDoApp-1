702ca619504605dc590aa5050d112cf16be77f12
NIMANZ <NIMA40480@GMAIL.COM>
2025-05-05 23:30:08 +0330

    description

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   basePath: '/ToDoApp-1',
   assetPrefix: '/ToDoApp-1/',
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
   },
   // Optional: Add trailing slash for GitHub Pages
   trailingSlash: true,
